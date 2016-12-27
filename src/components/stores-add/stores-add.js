85887b40236a91406fb62206918dd94e2f8a5306
Tim Combs <tim00combs@gmail.com>
2016-12-26 16:46:38 -0800

    add add store functionaliy - now adds a store to the database

--- src/components/stores-add/stores-add.js
+++ src/components/stores-add/stores-add.js
@@ -9,7 +9,24 @@ export default {
   controllerAs: '$storesAdd'
 };
 
-function controller() {
+controller.$inject = ['storeService', '$state'];
 
+function controller(stores, $state) {
 
+  this.addStore = () => {
+    stores.addStore({
+      name: this.name,
+      address: {
+        street: this.street,
+        city: this.city,
+        state: this.state
+      }
+    })
+    .then(saved => {
+      const savedId = saved._id;
+      this.stores.push(saved);
+      $state.go('store', {id: savedId});
+    });
+
+  };
 }
