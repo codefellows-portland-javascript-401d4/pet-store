bf968a480fb957efbbf870f80760f2bc6137d825
Tim Combs <tim00combs@gmail.com>
2016-12-27 12:56:50 -0800

    add /unauth to all /pets and /stores routes - cuz basic server routes updated to allow authorization

--- src/services/store-service.js
+++ src/services/store-service.js
@@ -3,15 +3,15 @@ storeService.$inject = ['$http', 'apiUrl'];
 export default function storeService($http, apiUrl) {
   return {
     getAll() {
-      return $http.get(`${apiUrl}/stores`)
+      return $http.get(`${apiUrl}/unauth/stores`)
         .then(res => res.data);
     },
     getOne(id) {
-      return $http.get(`${apiUrl}/stores/${id}`)
+      return $http.get(`${apiUrl}/unauth/stores/${id}`)
         .then(res => res.data);
     },
     addStore(store) {
-      return $http.post(`${apiUrl}/stores`, store)
+      return $http.post(`${apiUrl}/unauth/stores`, store)
         .then(res => res.data);
     }
 
