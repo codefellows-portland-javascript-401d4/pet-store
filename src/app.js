cd13e07bbc0a69191a695d50a981dc3d72fe60ed
DrewStock <andrewdstock@gmail.com>
2016-12-28 12:53:11 -0800

    added user auth

--- src/app.js
+++ src/app.js
@@ -3,17 +3,39 @@ import './scss/main.scss';
 
 import components from './components';
 import services from './services';
+
+import animate from 'angular-animate';
 import uiRouter from 'angular-ui-router';
+import defaultRoute from 'angular-ui-router-default';
+
+// need this for old $stateChanged events,
+// however, we need to manually grab the module
+// from angular (see below) as it is not
+// exported from this import
+import 'angular-ui-router/release/stateEvents';
+
+import dialog from 'ng-dialog';
+import 'ng-dialog/css/ngDialog.css';
+import 'ng-dialog/css/ngDialog-theme-default.css';
+
+import http from './http';
 import routes from './routes';
+import auth from './auth';
 
 const app = angular.module('myApp', [
   components,
   services,
-  uiRouter
+  animate,
+  uiRouter,
+  defaultRoute,
+  angular.module('ui.router.state.events').name,
+  dialog
 ]);
 
+// const dev = 'https://pet-store-401.herokuapp.com/api';
+
+app.value('apiUrl', 'https://pet-store-401.herokuapp.com/api');
+
+app.config(http);
 app.config(routes);
-
-const dev = 'https://pet-store-401.herokuapp.com/api/unauth';
-
-app.value('apiUrl', dev);
+app.run(auth);
--- src/components/store-app/store-app.js
+++ src/components/store-app/store-app.js
@@ -3,5 +3,14 @@ import styles from './store-app.scss';
 
 export default {
   template,
-  styles
+  styles,
+  controller
 };
+
+controller.$inject = ['userService'];
+
+function controller(userService) {
+  this.styles = styles;
+  this.logout = () => userService.logout();
+  this.isAuthenticated = () => userService.isAuthenticated();
+}
