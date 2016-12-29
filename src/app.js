c339b8cbb3a2a42d5b6dd45994841a2f81f44394
DrewStock <andrewdstock@gmail.com>
2016-12-28 17:11:30 -0800

    cleaned up styling

--- src/app.js
+++ src/app.js
@@ -8,10 +8,6 @@ import animate from 'angular-animate';
 import uiRouter from 'angular-ui-router';
 // import defaultRoute from 'angular-ui-router-default';
 
-// need this for old $stateChanged events,
-// however, we need to manually grab the module
-// from angular (see below) as it is not
-// exported from this import
 import 'angular-ui-router/release/stateEvents';
 
 import dialog from 'ng-dialog';
@@ -31,8 +27,6 @@ const app = angular.module('myApp', [
   angular.module('ui.router.state.events').name,
   dialog
 ]);
-
-// const dev = 'https://pet-store-401.herokuapp.com/api';
 
 app.value('apiUrl', 'https://pet-store-401.herokuapp.com/api');
 
