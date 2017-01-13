783ede19b3c0b17ef99d2e8762267405790c7886
mizutombo <thomas.shultz52@gmail.com>
2017-01-12 17:06:42 -0800

    initial code commit for lab2 ... need to validate functionity ... need to add tests

--- app/src/app.js
+++ app/src/app.js
@@ -4,17 +4,39 @@ import './scss/main.scss';
 //pick up index.js from components & services folder
 import components from './components';
 import services from './services';
+
+// 3rd party modules
 import uiRouter from 'angular-ui-router';
+
+// plug-in for 'default' routes
 import defaultRoute from 'angular-ui-router-default';
+
+// need this for old $stateChanged events;
+import 'angular-ui-router/release/stateEvents';
+
+import resource from 'angular-resource';
+
+import dialog from 'ng-dialog';
+import 'ng-dialog/css/ngDialog.css';
+import 'ng-dialog/css/ngDialog-theme-default.css';
+
+// route, http config, & auth setup
 import routes from './routes';
+import http from './auth/http';
+import auth from './auth/auth';
 
 const app = angular.module('petStore', [
   components,
   services,
   uiRouter,
-  defaultRoute
+  angular.module('ui.router.state.events').name,
+  defaultRoute,
+  dialog,
+  resource
 ]);
 
-app.config(routes);
-
 app.value('apiUrl', 'https://pet-store-401.herokuapp.com/api');
+
+app.config(routes);
+app.config(http);
+app.run(auth);
